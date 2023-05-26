873413194f8bfd75737937af2eb5a43b66f58247
Lanooo <helanoprado492@gmail.com>
2023-05-26 08:48:09 -0300

    ultima aula
    26/06

--- itapira/enquete/urls.py
+++ itapira/enquete/urls.py
@@ -1,15 +1,14 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
-from.import views
+from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('musica', views.caneta, name='caneta'),
-     # ex: /enquete/5
-    path("<int:questao_id>/", views.detalhe, name="detalhes"),
-     # ex: /enquete/5/resultados
+    path('felipe', views.caneta, name='caneta'),
+     # ex: /enquete/5/
+    path("<int:questao_id>/", views.detalhe, name="detalhe"),
+    # ex: /enquete/5/resultados/
     path("<int:questao_id>/resultados/", views.resultados, name="resultados"),
-     # ex: /enquete/5/voto
+    # ex: /enquete/5/voto/
     path("<int:questao_id>/voto/", views.voto, name="voto"),
 ]
--- itapira/enquete/views.py
+++ itapira/enquete/views.py
@@ -1,23 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Questao
 
 # Create your views here.
 
+# explicar isso semana que vem
 def index(request):
     ultimas_questoes = Questao.objects.order_by("-data")[:5]
-    saida = ",".join([q.questao for q in ultimas_questoes])
+    saida = ", ".join([q.pergunta for q in ultimas_questoes])
+    saida = ""
+    for q in ultimas_questoes:
+        linha =
     return HttpResponse(saida)
 
+
 def caneta(request):
-    return HttpResponse("<h1>Olha, se você não me ama</h1><h2>Caneta azul</h2>")
+    return HttpResponse("<h1>Caneta Azul, Azul caneta...</h1>")
 
 
 def detalhe(request, questao_id):
-    return HttpResponse("Você está olhando a questao %s." % questao_id)
-    
+    return HttpResponse("Você está olhando a questão %s." % questao_id)
+
+
 def resultados(request, questao_id):
     response = "Você está olhando o resultado da questão %s."
-    return HttpResponse( response % questao_id)
+    return HttpResponse(response % questao_id)
+
 
 def voto(request, questao_id):
-    return HttpResponse( "Você está votando na questão %s." % questao_id)
+    return HttpResponse("Você está votando na questão %s." % questao_id)
